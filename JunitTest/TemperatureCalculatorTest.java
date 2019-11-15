65f1b420a8f5e4b1fa05951de87d1be594022d7f
Touhidul Islam <russel2005@gmail.com>
2019-11-15 10:22:56 -0500

    Create TemperatureCalculatorTest.java

---
+++ JunitTest/TemperatureCalculatorTest.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TemperatureCalculatorTest {
+	
+	@Test
+    public void testToDegree(){
+        TemperatureCalculator calculator = new TemperatureCalculator();
+        assertEquals(36.666668f,calculator.toDegree(98),0);
+    }
+ 
+    @Test
+    public void testToFahrenheit(){
+        TemperatureCalculator calculator = new TemperatureCalculator();
+        assertEquals("Test failed for Degree to Fahrenheit",212f,calculator.toFahrenheit(100),0);
+    }
+}
