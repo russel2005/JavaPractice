e4701246b347308f01f0deada8149905c229a511
Touhidul Islam <russel2005@gmail.com>
2020-02-19 16:57:41 -0500

    Update PracticeCode02.java

--- InterviewCodePractice/PracticeCode02.java
+++ InterviewCodePractice/PracticeCode02.java
@@ -143,11 +143,11 @@ for (int i = 1; i <= num; i++){
     int counter =0; 	  
     for(int j =i; j>=1; j--){
         if(i%j==0) {
-	  counter = counter + 1;
+	  counter += 1;
 	}
     }
     if (counter ==2) {		     
-	primeNumbers = primeNumbers + i + " ";
+	primeNumbers += i + " ";
     }	
 }	
 System.out.println("Prime numbers from 1 to 100 are :");
